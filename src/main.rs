278293b69ebc1cfac3643c79f8290d91f9677ad6
Michael Dorst <m@mdorst.net>
2020-01-25 13:23:19 -0800

    Create lexer example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,63 @@
 #![allow(non_snake_case)]
 
+use logos::Logos;
+
+#[derive(Logos, Debug)]
+enum Token {
+  #[end]
+  End,
+
+  #[error]
+  Error,
+
+  #[token = "let"]
+  Let,
+
+  #[token = "="]
+  Assign,
+
+  #[regex = "[a-zA-Z_][a-zA-Z0-9_]*"]
+  Ident,
+
+  #[regex = "[0-9]+"]
+  Integer,
+
+  #[token = "\n"]
+  Newline,
+}
+
 fn main() {
-  println!("Hello, world!");
+  let mut lexer = Token::lexer("let x = 5\nlet y = 3");
+
+  println!("{:?}", lexer.token);
+  println!("{:?}", lexer.slice());
+  println!("{:?}", lexer.range());
+  lexer.advance();
+  println!("{:?}", lexer.token);
+  println!("{:?}", lexer.slice());
+  println!("{:?}", lexer.range());
+  lexer.advance();
+  println!("{:?}", lexer.token);
+  println!("{:?}", lexer.slice());
+  println!("{:?}", lexer.range());
+  lexer.advance();
+  println!("{:?}", lexer.token);
+  println!("{:?}", lexer.slice());
+  println!("{:?}", lexer.range());
+  lexer.advance();
+  println!("{:?}", lexer.token);
+  println!("{:?}", lexer.slice());
+  println!("{:?}", lexer.range());
+  lexer.advance();
+  println!("{:?}", lexer.token);
+  println!("{:?}", lexer.slice());
+  println!("{:?}", lexer.range());
+  lexer.advance();
+  println!("{:?}", lexer.token);
+  println!("{:?}", lexer.slice());
+  println!("{:?}", lexer.range());
+  lexer.advance();
+  println!("{:?}", lexer.token);
+  println!("{:?}", lexer.slice());
+  println!("{:?}", lexer.range());
 }
