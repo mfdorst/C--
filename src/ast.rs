b38244dff313993c41010c4d3210fa1b1377e60f
Michael Dorst <m@mdorst.net>
2020-02-06 19:02:18 -0800

    Make Ast a single enum

--- src/ast.rs
+++ src/ast.rs
@@ -3,28 +3,18 @@ use crate::parser;
 use parser::{Expr, ExprPrime, Factor, Term, TermPrime};
 use std::fmt::Debug;
 
-pub trait Ast: Debug {}
-
 #[derive(Debug)]
-pub enum Op {
-  Add(Box<dyn Ast>, Box<dyn Ast>),
-  Sub(Box<dyn Ast>, Box<dyn Ast>),
-  Mul(Box<dyn Ast>, Box<dyn Ast>),
-  Div(Box<dyn Ast>, Box<dyn Ast>),
-}
-
-impl Ast for Op {}
-
-#[derive(Debug)]
-pub enum Value {
+pub enum Ast {
+  Add(Box<Ast>, Box<Ast>),
+  Sub(Box<Ast>, Box<Ast>),
+  Mul(Box<Ast>, Box<Ast>),
+  Div(Box<Ast>, Box<Ast>),
   Ident(String),
   Integer(i32),
 }
 
-impl Ast for Value {}
-
 impl Expr {
-  pub fn to_ast(&self) -> Box<dyn Ast> {
+  pub fn to_ast(&self) -> Box<Ast> {
     match self {
       Expr::Expr(term, expr_pr) => expr_pr.to_ast(term),
     }
@@ -32,17 +22,17 @@ impl Expr {
 }
 
 impl ExprPrime {
-  fn to_ast(&self, lterm: &Term) -> Box<dyn Ast> {
+  fn to_ast(&self, lterm: &Term) -> Box<Ast> {
     match self {
-      ExprPrime::Add(rterm, expr_pr) => Box::new(Op::Add(lterm.to_ast(), expr_pr.to_ast(rterm))),
-      ExprPrime::Sub(rterm, expr_pr) => Box::new(Op::Sub(lterm.to_ast(), expr_pr.to_ast(rterm))),
+      ExprPrime::Add(rterm, expr_pr) => Box::new(Ast::Add(lterm.to_ast(), expr_pr.to_ast(rterm))),
+      ExprPrime::Sub(rterm, expr_pr) => Box::new(Ast::Sub(lterm.to_ast(), expr_pr.to_ast(rterm))),
       ExprPrime::None => lterm.to_ast(),
     }
   }
 }
 
 impl Term {
-  fn to_ast(&self) -> Box<dyn Ast> {
+  fn to_ast(&self) -> Box<Ast> {
     match self {
       Term::Term(factor, term_pr) => term_pr.to_ast(factor),
     }
@@ -50,13 +40,13 @@ impl Term {
 }
 
 impl TermPrime {
-  fn to_ast(&self, lfactor: &Factor) -> Box<dyn Ast> {
+  fn to_ast(&self, lfactor: &Factor) -> Box<Ast> {
     match self {
       TermPrime::Mult(rfactor, term_pr) => {
-        Box::new(Op::Mul(lfactor.to_ast(), term_pr.to_ast(rfactor)))
+        Box::new(Ast::Mul(lfactor.to_ast(), term_pr.to_ast(rfactor)))
       }
       TermPrime::Div(rfactor, term_pr) => {
-        Box::new(Op::Div(lfactor.to_ast(), term_pr.to_ast(rfactor)))
+        Box::new(Ast::Div(lfactor.to_ast(), term_pr.to_ast(rfactor)))
       }
       TermPrime::None => lfactor.to_ast(),
     }
@@ -64,10 +54,10 @@ impl TermPrime {
 }
 
 impl Factor {
-  fn to_ast(&self) -> Box<dyn Ast> {
+  fn to_ast(&self) -> Box<Ast> {
     match self {
-      Factor::Ident(id) => Box::new(Value::Ident(id.to_owned())),
-      Factor::Integer(i) => Box::new(Value::Integer(*i)),
+      Factor::Ident(id) => Box::new(Ast::Ident(id.to_owned())),
+      Factor::Integer(i) => Box::new(Ast::Integer(*i)),
       Factor::Expr(expr) => expr.to_ast(),
     }
   }
